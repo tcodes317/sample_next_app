10be326daf66df7d6a4338185214b70f1f8df1f1
Codes317 <120498081+tcodes317@users.noreply.github.com>
2024-07-11 21:29:42 +0000

    7/11/2024

--- my-app/components/Header.jsx
+++ my-app/components/Header.jsx
@@ -5,8 +5,17 @@ import {Button} from "./ui/button";
 const Header = () => {
     return(
         <>
-            <header className="py-8">
-                <div className="container mx-auto">Header</div>
+            <header className="py-8 xl:py-12 text-white bg-pink-50/20">
+                <div className="container mx-auto">
+
+                    {/** logo */}
+                    <Link href="">
+                        <h1 className="text-4xl font-semibold">Luke <span className="text-accent">.</span></h1>
+                    </Link>
+
+                    {/** desktop nav */}
+                    <Nav />
+                </div>
             </header>
         </>
     )
