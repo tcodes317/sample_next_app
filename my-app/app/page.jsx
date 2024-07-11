b58db4fbfb63ef392d7f3929628ded10109b9e89
Codes317 <120498081+tcodes317@users.noreply.github.com>
2024-07-11 15:04:29 +0000

    7/1/2024

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="overflow-hidden">
+      <div>Header</div>
+      <div>Main</div>
+      <div>Footer</div>
     </main>
   );
 }
